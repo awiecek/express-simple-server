7d7732c42b6232cb589e20dd1e84b3bd405d241a
Adrian Wiecek <adrian.wiecek@wpengine.com>
2021-08-31 16:28:01 +0200

    build message with ansi colors

--- build.js
+++ build.js
@@ -1,2 +1,6 @@
-console.log('MY_SECRET: ', process.env.MY_SECRET);
-console.log('ALL ENVS: ', process.env);
+const colors = require("colors/safe");
+
+console.log(colors.green("WPE BUILD START"));
+console.log("MY_SECRET: ", colors.magenta(process.env.MY_SECRET));
+console.log("ALL ENVS: ", colors.blue(process.env));
+console.log(colors.green("WPE BUILD END"));
