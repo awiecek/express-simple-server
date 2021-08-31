e137bfe0382b89538ed3fa0736513cc65a63d16c
Adrian Wiecek <adrian.wiecek@wpengine.com>
2021-08-31 16:33:38 +0200

    sample build error

--- build.js
+++ build.js
@@ -3,4 +3,7 @@ const colors = require("colors/safe");
 console.log(colors.green("WPE BUILD START"));
 console.log("MY_SECRET: ", colors.magenta(process.env.MY_SECRET));
 console.log("ALL ENVS: ", colors.blue(process.env));
+
+console.error(colors.red("This is just a sample error during build"));
+
 console.log(colors.green("WPE BUILD END"));
