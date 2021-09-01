8d65dedb6cddd0ef585c46a1bdad6b975367d161
Adrian Wiecek <adrian.wiecek@wpengine.com>
2021-09-01 15:48:08 +0200

    addind a code that causes RTE during wpe-build

--- build.js
+++ build.js
@@ -1,5 +1,10 @@
 const colors = require("colors/safe");
 
+// the below should fail with RTE
+(function () {
+  fs.read();
+})();
+
 console.log(colors.green("WPE BUILD START"));
 console.log("MY_SECRET: ", colors.magenta(process.env.MY_SECRET));
 console.log("ALL ENVS: ", colors.blue(process.env));
