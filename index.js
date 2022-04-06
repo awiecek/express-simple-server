d50e33a084f61feac0d61a6e59fbc85c876672e3
Adrian Wiecek <85167107+awiecek@users.noreply.github.com>
2022-04-06 11:32:29 +0200

    Update index.js

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const PORT = process.env.PORT || 3300;
 server.use(express.static("public"));
 
 server.get("/", (_req, res) => {
-  res.send("Hello Express! 1");
+  res.send("Hello Express!");
 });
 
 server.get("/fetch-wordpress-graphql", async (_req, res) => {
